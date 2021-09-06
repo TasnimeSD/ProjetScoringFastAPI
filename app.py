341bfc107a6c6369e5963c2253dca09b7dbd06db
TasnimeSD <37716073+TasnimeSD@users.noreply.github.com>
2021-09-06 11:58:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -126,5 +126,7 @@ async def predict(request: Request):
 
 
 if __name__ == '__main__':   
-    uvicorn.run("app:app", host='127.0.0.1', port=8000, reload=True)
+    port = int(os.environ.get('PORT', 8000))
+    uvicorn.run("app:app", host='0.0.0.0', port=port, reload=True)
+    #uvicorn.run("app:app", host='127.0.0.1', port=8000, reload=True)
 
